c1a2299fec9b233974ec3062cb58fa45662cae8b
Michel <michel@mbp-de-michel.home>
2022-04-13 22:53:47 +0200

    login done

--- server/routesLogin.mjs
+++ server/routesLogin.mjs
@@ -3,14 +3,19 @@ import pool from './services/config.mjs'
 
 const routerLogin = express.Router()
 const sqlUser = 'SELECT * FROM users WHERE pseudo = $1 AND password = $2'
+var errorIncorrectInfo = ""
 
-routerLogin.get('/', (req, res) => {
-    res.render('login')
+routerLogin.get('/', (_, res) => {
+    res.render('login', {
+        errorMessage: ""
+    })
 })
 
 routerLogin.post('/auth', (req, res) => {
     let username = req.body.pseudo
     let password = req.body.password
+
+    errorIncorrectInfo = ""
 
     if (username && password) {
         pool.query(sqlUser, [username, password], function (err, results) {
@@ -18,13 +23,17 @@ routerLogin.post('/auth', (req, res) => {
             if (results.rows.length > 0) {
                 res.redirect('/home')
             } else {
-                res.send('Incorrect username and/or password')
+                res.render('login', {
+                    errorMessage: "Incorrect username and/or password"
+                })
             }
             res.end
         })
         pool.end
     } else {
-        res.send("Please enter a username and password")
+        res.render('login', {
+            errorMessage: "Please enter a username and password"
+        })
         res.end
     }
 })
