50045863f43aa4c9d10485b5c3b417c77dea25b1
Michel <michel@mbp-de-michel.home>
2022-04-17 13:58:43 +0200

    set the contact page

--- server/routesContacts.mjs
+++ server/routesContacts.mjs
@@ -1,21 +1,33 @@
 import express from 'express'
 import pool from './services/config.mjs'
+import fs from 'fs'
 
 const routerContact = express.Router()
 const sqlContact = "SELECT * FROM contact"
 
 routerContact.get('/contacts', (req, res) => {
     try {
+        var infoUser = retrieveUsername()
         pool.query(sqlContact, [], (err, result) => {
             if (err) {
                 return console.error(err.message)
             }
             req.body = result.rows
-            res.render("contacts", { model: result.rows })
+            res.render("contacts", { getName: infoUser })
         })
     } catch (err) {
         console.error('Error in routerContacts with get method : /contacts', err.message)
     }
 })
+
+function retrieveUsername() {
+    var data = fs.readFileSync('./configUser.json', 'utf8')
+    try {
+        var dataParse = JSON.parse(data)
+        return dataParse['username']
+    } catch (err) {
+        console.error(err)
+    }
+}
 
 export default routerContact
