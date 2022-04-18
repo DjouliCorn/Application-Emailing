5c1418dfd10f40172713659615f538cf3ea5a30a
Michel <michel@mbp-de-michel.home>
2022-04-18 17:03:27 +0200

    in progress contact

--- server/routesContacts.mjs
+++ server/routesContacts.mjs
@@ -13,18 +13,30 @@ routerContact.get('/contacts', (req, res) => {
                 return console.error(err.message)
             }
             let jsonData = JSON.stringify(result.rows)
-            res.render("contacts", { getName: infoUser, resultModule: jsonData })
+            res.render("allContacts", { getName: infoUser, resultModule: jsonData })
         })
     } catch (err) {
         console.error('Error in routerContacts with get method : /contacts', err.message)
     }
 })
 
+routerContact.get('/contacts&idContact=')
+
 function retrieveUsername() {
     var data = fs.readFileSync('./configUser.json', 'utf8')
     try {
         var dataParse = JSON.parse(data)
         return dataParse['username']
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+function retrieveIdUser() {
+    var data = fs.readFileSync('./configUser.json', 'utf8')
+    try {
+        var dataParse = JSON.parse(data)
+        return dataParse['id']
     } catch (err) {
         console.error(err)
     }
